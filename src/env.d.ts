18bc9a418ae87e9e39a509ba7ccbdf4477855191
EthanZhang <hi@itcox.cn>
2026-05-27 20:36:49 +0800

    Declare deployment env process type

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="node" />
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
   readonly DEPLOYED_AT?: string;
 }
+
+declare const process: {
+  readonly env: {
+    readonly DEPLOYED_AT?: string;
+  };
+};
